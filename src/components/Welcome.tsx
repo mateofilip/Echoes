cdf7100a8ecde3082fce2a16c8968776777ee623
mateofilip <mateofernandofilip@gmail.com>
2025-07-08 15:49:30 -0300

    Reworked color scheme + button behavior under certains circumstances (dark mode, disabled, etc.)

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -87,23 +87,6 @@ export default function Welcome() {
       </h1>
       <main className="flex h-dvh w-dvw flex-col justify-center px-5 sm:px-16 md:px-28 lg:px-52 xl:px-96 2xl:px-120">
         <div className="flex h-dvh flex-col justify-center gap-30">
-          {/* <span className="flex scale-x-[-1] justify-end text-8xl">
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className=""
-          >
-            <path
-              d="M9.42503 3.44136C10.0561 3.23654 10.7837 3.2402 11.3792 3.54623C12.7532 4.25224 13.3477 6.07191 12.7946 8C12.5465 8.8649 12.1102 9.70472 11.1861 10.5524C10.262 11.4 8.98034 11.9 8.38571 11.9C8.17269 11.9 8 11.7321 8 11.525C8 11.3179 8.17644 11.15 8.38571 11.15C9.06497 11.15 9.67189 10.7804 10.3906 10.236C10.9406 9.8193 11.3701 9.28633 11.608 8.82191C12.0628 7.93367 12.0782 6.68174 11.3433 6.34901C10.9904 6.73455 10.5295 6.95946 9.97725 6.95946C8.7773 6.95946 8.0701 5.99412 8.10051 5.12009C8.12957 4.28474 8.66032 3.68954 9.42503 3.44136ZM3.42503 3.44136C4.05614 3.23654 4.78366 3.2402 5.37923 3.54623C6.7532 4.25224 7.34766 6.07191 6.79462 8C6.54654 8.8649 6.11019 9.70472 5.1861 10.5524C4.26201 11.4 2.98034 11.9 2.38571 11.9C2.17269 11.9 2 11.7321 2 11.525C2 11.3179 2.17644 11.15 2.38571 11.15C3.06497 11.15 3.67189 10.7804 4.39058 10.236C4.94065 9.8193 5.37014 9.28633 5.60797 8.82191C6.06282 7.93367 6.07821 6.68174 5.3433 6.34901C4.99037 6.73455 4.52948 6.95946 3.97725 6.95946C2.7773 6.95946 2.0701 5.99412 2.10051 5.12009C2.12957 4.28474 2.66032 3.68954 3.42503 3.44136Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </span> */}
           <div className="flex flex-col justify-between gap-20">
             <p
               className={`text-center text-4xl font-light transition-all duration-500 ease-in-out ${reloading ? "opacity-0" : "opacity-100"}`}
@@ -116,29 +99,11 @@ export default function Welcome() {
               — {quotes[currentIndex]?.author || "Loading..."}
             </h2>
           </div>
-
-          {/* <span className="text-8x flex justify-end">
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 15 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className=""
-          >
-            <path
-              d="M9.42503 3.44136C10.0561 3.23654 10.7837 3.2402 11.3792 3.54623C12.7532 4.25224 13.3477 6.07191 12.7946 8C12.5465 8.8649 12.1102 9.70472 11.1861 10.5524C10.262 11.4 8.98034 11.9 8.38571 11.9C8.17269 11.9 8 11.7321 8 11.525C8 11.3179 8.17644 11.15 8.38571 11.15C9.06497 11.15 9.67189 10.7804 10.3906 10.236C10.9406 9.8193 11.3701 9.28633 11.608 8.82191C12.0628 7.93367 12.0782 6.68174 11.3433 6.34901C10.9904 6.73455 10.5295 6.95946 9.97725 6.95946C8.7773 6.95946 8.0701 5.99412 8.10051 5.12009C8.12957 4.28474 8.66032 3.68954 9.42503 3.44136ZM3.42503 3.44136C4.05614 3.23654 4.78366 3.2402 5.37923 3.54623C6.7532 4.25224 7.34766 6.07191 6.79462 8C6.54654 8.8649 6.11019 9.70472 5.1861 10.5524C4.26201 11.4 2.98034 11.9 2.38571 11.9C2.17269 11.9 2 11.7321 2 11.525C2 11.3179 2.17644 11.15 2.38571 11.15C3.06497 11.15 3.67189 10.7804 4.39058 10.236C4.94065 9.8193 5.37014 9.28633 5.60797 8.82191C6.06282 7.93367 6.07821 6.68174 5.3433 6.34901C4.99037 6.73455 4.52948 6.95946 3.97725 6.95946C2.7773 6.95946 2.0701 5.99412 2.10051 5.12009C2.12957 4.28474 2.66032 3.68954 3.42503 3.44136Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </span> */}
         </div>
         <div className="fixed right-0 bottom-0 left-0 mb-15 flex justify-center gap-2">
           <button
             onClick={getQuote}
-            className="group relative w-fit cursor-pointer rounded-sm border-1 border-gray-300 p-3"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 dark:border-orange-200"
           >
             <svg
               width="20"
@@ -162,7 +127,7 @@ export default function Welcome() {
           </button>
 
           <button
-            className="group relative w-fit cursor-pointer rounded-sm border-1 border-gray-300 p-3"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 dark:border-orange-200"
             onClick={toggleTheme}
           >
             <span className="relative block h-5 w-5">
@@ -213,7 +178,7 @@ export default function Welcome() {
 
           <button
             onClick={getPreviousQuote}
-            className="group relative w-fit cursor-pointer rounded-sm border-1 border-gray-300 p-3 transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:text-gray-300"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400 dark:border-orange-200"
             disabled={currentIndex === quotes.length - 1}
           >
             <svg
@@ -239,7 +204,7 @@ export default function Welcome() {
 
           <button
             onClick={getNextQuote}
-            className="group relative w-fit cursor-pointer rounded-sm border-1 border-gray-300 p-3 transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:text-gray-300"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400 dark:border-orange-200"
             disabled={currentIndex === 0}
           >
             <svg
