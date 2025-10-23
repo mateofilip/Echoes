d652384f6c8c9bce65cce7c1f1797622428c9e4f
mateofilip <mateofernandofilip@gmail.com>
2025-10-23 16:37:25 -0300

    Modified animation easing and duration to make the site feel snappier

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -106,7 +106,7 @@ export default function Welcome() {
         <div className="flex h-dvh flex-col justify-center gap-30">
           <div className="flex flex-col justify-between gap-20">
             <p
-              className={`variableSize text-center transition-all duration-300 ease-in-out ${reloading ? "opacity-0" : "opacity-100"}`}
+              className={`variableSize text-center transition-all duration-200 ease-out ${reloading ? "opacity-0" : "opacity-100"}`}
             >
               <span>«&nbsp;</span>
               {quotes[currentIndex]?.quote
@@ -115,7 +115,7 @@ export default function Welcome() {
               <span>&nbsp;»</span>
             </p>
             <h2
-              className={`text-right text-xl transition-all duration-300 ease-in-out md:text-2xl ${reloading ? "opacity-0" : "opacity-100"}`}
+              className={`text-right text-xl transition-all duration-200 ease-out md:text-2xl ${reloading ? "opacity-0" : "opacity-100"}`}
             >
               — {quotes[currentIndex]?.author || "Loading..."}
             </h2>
@@ -124,7 +124,7 @@ export default function Welcome() {
         <div className="fixed right-0 bottom-0 left-0 mb-30 flex justify-center gap-2">
           <button
             onClick={getQuote}
-            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg dark:border-orange-200"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-200 ease-out hover:scale-105 hover:shadow-lg dark:border-orange-200"
           >
             <svg
               width="20"
@@ -132,7 +132,7 @@ export default function Welcome() {
               viewBox="0 0 15 15"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className={`${isReloadingButton ? "rotate-240" : ""} transition-all duration-300`}
+              className={`${isReloadingButton ? "rotate-240" : ""} transition-all duration-200`}
             >
               <path
                 d="M1.84998 7.49998C1.84998 4.66458 4.05979 1.84998 7.49998 1.84998C10.2783 1.84998 11.6515 3.9064 12.2367 5H10.5C10.2239 5 10 5.22386 10 5.5C10 5.77614 10.2239 6 10.5 6H13.5C13.7761 6 14 5.77614 14 5.5V2.5C14 2.22386 13.7761 2 13.5 2C13.2239 2 13 2.22386 13 2.5V4.31318C12.2955 3.07126 10.6659 0.849976 7.49998 0.849976C3.43716 0.849976 0.849976 4.18537 0.849976 7.49998C0.849976 10.8146 3.43716 14.15 7.49998 14.15C9.44382 14.15 11.0622 13.3808 12.2145 12.2084C12.8315 11.5806 13.3133 10.839 13.6418 10.0407C13.7469 9.78536 13.6251 9.49315 13.3698 9.38806C13.1144 9.28296 12.8222 9.40478 12.7171 9.66014C12.4363 10.3425 12.0251 10.9745 11.5013 11.5074C10.5295 12.4963 9.16504 13.15 7.49998 13.15C4.05979 13.15 1.84998 10.3354 1.84998 7.49998Z"
@@ -141,14 +141,14 @@ export default function Welcome() {
                 clipRule="evenodd"
               ></path>
             </svg>
-            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-300 ease-in-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
+            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-200 ease-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
               Get a New Quote
               <span className="absolute top-full left-1/2 h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-b-0 border-x-transparent border-t-stone-900"></span>
             </span>
           </button>
 
           <button
-            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg dark:border-orange-200"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-200 ease-out hover:scale-105 hover:shadow-lg dark:border-orange-200"
             onClick={toggleTheme}
           >
             <span className="relative block h-5 w-5">
@@ -158,7 +158,7 @@ export default function Welcome() {
                 viewBox="0 0 15 15"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className={`absolute top-0 left-0 transition-all duration-300 ${
+                className={`absolute top-0 left-0 transition-all duration-200 ${
                   isDark
                     ? "scale-75 rotate-90 opacity-0"
                     : "scale-100 rotate-0 opacity-100"
@@ -177,7 +177,7 @@ export default function Welcome() {
                 viewBox="0 0 15 15"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className={`absolute top-0 left-0 transition-all duration-300 ${
+                className={`absolute top-0 left-0 transition-all duration-200 ${
                   isDark
                     ? "scale-100 rotate-0 opacity-100"
                     : "scale-75 -rotate-90 opacity-0"
@@ -191,7 +191,7 @@ export default function Welcome() {
                 ></path>
               </svg>
             </span>
-            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-300 ease-in-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
+            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-200 ease-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
               Toggle Dark Mode
               <span className="absolute top-full left-1/2 h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-b-0 border-x-transparent border-t-stone-900"></span>
             </span>
@@ -199,7 +199,7 @@ export default function Welcome() {
 
           <button
             onClick={getPreviousQuote}
-            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400 disabled:hover:scale-none disabled:hover:shadow-none dark:border-orange-200"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-200 ease-out hover:scale-105 hover:shadow-lg disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400 disabled:hover:scale-none disabled:hover:shadow-none dark:border-orange-200"
             disabled={currentIndex === quotes.length - 1}
           >
             <svg
@@ -208,7 +208,7 @@ export default function Welcome() {
               viewBox="0 0 15 15"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className={`transition-transform duration-300 ${isPrevAnimating ? "scale-125" : "scale-100"}`}
+              className={`transition-transform duration-200 ${isPrevAnimating ? "scale-125" : "scale-100"}`}
             >
               <path
                 d="M4.85355 2.14645C5.04882 2.34171 5.04882 2.65829 4.85355 2.85355L3.70711 4H9C11.4853 4 13.5 6.01472 13.5 8.5C13.5 10.9853 11.4853 13 9 13H5C4.72386 13 4.5 12.7761 4.5 12.5C4.5 12.2239 4.72386 12 5 12H9C10.933 12 12.5 10.433 12.5 8.5C12.5 6.567 10.933 5 9 5H3.70711L4.85355 6.14645C5.04882 6.34171 5.04882 6.65829 4.85355 6.85355C4.65829 7.04882 4.34171 7.04882 4.14645 6.85355L2.14645 4.85355C1.95118 4.65829 1.95118 4.34171 2.14645 4.14645L4.14645 2.14645C4.34171 1.95118 4.65829 1.95118 4.85355 2.14645Z"
@@ -217,7 +217,7 @@ export default function Welcome() {
                 clipRule="evenodd"
               ></path>
             </svg>
-            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-300 ease-in-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
+            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-200 ease-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
               Go Back to Previous Quote
               <span className="absolute top-full left-1/2 h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-b-0 border-x-transparent border-t-stone-900"></span>
             </span>
@@ -225,7 +225,7 @@ export default function Welcome() {
 
           <button
             onClick={getNextQuote}
-            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400 disabled:hover:scale-none disabled:hover:shadow-none dark:border-orange-200"
+            className="group relative w-fit cursor-pointer rounded-lg border-1 border-gray-700 p-3 shadow-none transition-all duration-200 ease-out hover:scale-105 hover:shadow-lg disabled:cursor-not-allowed disabled:border-gray-400 disabled:text-gray-400 disabled:hover:scale-none disabled:hover:shadow-none dark:border-orange-200"
             disabled={currentIndex === 0}
           >
             <svg
@@ -234,7 +234,7 @@ export default function Welcome() {
               viewBox="0 0 15 15"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className={`transition-transform duration-300 ${isNextAnimating ? "scale-125" : "scale-100"}`}
+              className={`transition-transform duration-200 ${isNextAnimating ? "scale-125" : "scale-100"}`}
             >
               <path
                 d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
@@ -244,7 +244,7 @@ export default function Welcome() {
               ></path>
             </svg>
 
-            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-300 ease-in-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
+            <span className="Alte absolute -top-15 left-1/2 z-10 -translate-x-1/2 translate-y-2 rounded-2xl border border-stone-700 bg-stone-900 p-3 text-xs whitespace-nowrap text-orange-50 opacity-0 shadow-xl transition-all duration-200 ease-out group-hover:-translate-y-0 group-hover:opacity-100 group-hover:delay-300">
               Go to the Next Quote
               <span className="absolute top-full left-1/2 h-0 w-0 -translate-x-1/2 border-x-8 border-t-8 border-b-0 border-x-transparent border-t-stone-900"></span>
             </span>
