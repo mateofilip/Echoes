55e7db9ee78bdb6d36ec8014e0bfec0024b53995
Mateo Filip <mateofernandofilip@gmail.com>
2026-09-02 16:36:30 -0300

    fix: silence supabase auth lock error; add unknown author images

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false,
+  },
+});
